bac272c091dccebe63d1b533b38ad8925377d203
ed-kung <edward.kung@gmail.com>
2023-10-17 14:23:58 -0700

    Update project example

--- data/homeownership/homeownership.R
+++ data/homeownership/homeownership.R
@@ -1,4 +1,10 @@
 rm(list=ls())
+
+# We only need one function (wtd.var) from Hmisc
+# If we load the whole thing, we'll have conflicts with dplyr
+library(Hmisc, include.only="wtd.var")  
+
+# Load the rest of the libraries
 library(dplyr)
 library(ggplot2)
 library(lfe)
@@ -30,31 +36,67 @@ ggplot(data=df_own_by_age) +
   geom_line(aes(x=AGE, y=HOMEOWNERSHIP_RATE, color=YEAR)) +
   xlab("Age") + 
   ylab("Homeownership Rate") + 
-  ggtitle("Homeownership Rate by Age, 1980 vs 2021")
-
-
-# Create dummy variables for age bands
-df$AGE_20_29 <- df$AGE>=20 & df$AGE<30
-df$AGE_30_39 <- df$AGE>=30 & df$AGE<40
-df$AGE_40_49 <- df$AGE>=40 & df$AGE<50
-df$AGE_50_59 <- df$AGE>=50 & df$AGE<60
-df$AGE_60_69 <- df$AGE>=60 & df$AGE<70
-df$AGE_70_79 <- df$AGE>=70 & df$AGE<80
-df$AGE_80_90 <- df$AGE>=80 & df$AGE<=90
+  ggtitle("Figure 1: Homeownership Rate by Age, 1980 vs 2021") + 
+  theme_bw()
 
 # Clean up the income variable
 df$HHINCOME <- ifelse(df$HHINCOME==9999999, NA, df$HHINCOME)
 df$HHINCOME <- ifelse(df$HHINCOME<1, 1, df$HHINCOME)
 
-# Create other useful variables
+
+# Create other regression variables
 df$AGE_SQ <- df$AGE*df$AGE/100
 df$MARRIED <- df$MARST==1
 df$MALE <- df$SEX==1
 df$COLLEGE <- df$EDUC>=10
 df$WHITE <- df$RACE==1 
 
-# Create some other demographic variables
+# Create a summary table
+# Note: This is not an efficient way to do it, but I want to use a 
+# method that the students would already have had exposure to
 
+# SUMMARIZE OWN
+df %>% group_by(YEAR) %>% summarize(
+  MEAN = weighted.mean(OWN, HHWT)
+)
+
+# SUMMARIZE AGE
+df %>% group_by(YEAR) %>% summarize(
+  MIN = min(AGE),
+  MAX = max(AGE),
+  MEAN = weighted.mean(AGE, HHWT),
+  STD = sqrt(wtd.var(AGE,HHWT))
+)
+
+# SUMMARIZE HHINCOME
+df %>% group_by(YEAR) %>% summarize(
+  MIN = min(HHINCOME,na.rm=T),
+  MAX = max(HHINCOME,na.rm=T),
+  MEAN = weighted.mean(HHINCOME,HHWT,na.rm=T),
+  STD = sqrt(wtd.var(HHINCOME,HHWT,na.rm=T))
+)
+
+# SUMMARIZE MARRIED
+df %>% group_by(YEAR) %>% summarize(
+  MEAN = weighted.mean(MARRIED, HHWT)
+)
+
+# SUMMARIZE MALE
+df %>% group_by(YEAR) %>% summarize(
+  MEAN = weighted.mean(MALE, HHWT)
+)
+
+# SUMMARIZE COLLEGE
+df %>% group_by(YEAR) %>% summarize(
+  MEAN = weighted.mean(COLLEGE, HHWT)
+)
+
+# SUMMARIZE WHITE
+df %>% group_by(YEAR) %>% summarize(
+  MEAN = weighted.mean(WHITE, HHWT)
+)
+
+# Run the regressions
 r1980 <- felm(
   OWN ~ AGE + AGE_SQ + log(HHINCOME) + MARRIED + MALE + COLLEGE + WHITE,
   data=filter(df, YEAR==1980),
@@ -68,5 +110,6 @@ r2021 <- felm(
   weights=filter(df, YEAR==2021)$HHWT
 )
 
-stargazer(r1980, r2021, type="text")
+stargazer(r1980, r2021, type="text",
+          column.labels=c("1980", "2021"))
 
