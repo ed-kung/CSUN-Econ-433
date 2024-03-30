1747239ab7888d06ebd17bf9bab67157b1052173
Edward Kung <edward.kung@gusto.com>
2024-03-30 14:36:02 -0700

    add workspace data and files

---
+++ _workspace/create_IPUMS_ACS2019_CA_data.R
@@ -0,0 +1,36 @@
+rm(list=ls())
+
+library(dplyr)
+
+df <- read.csv("IPUMS_ACS2019_CA_raw.csv")
+
+df1 <- df %>% select(
+  YEAR,
+  SERIAL,
+  PERNUM,
+  PERWT,
+  STATEFIP,
+  COUNTYFIP,
+  AGE,
+  SEX,
+  MARST,
+  RACHSING,
+  EMPSTAT,
+  INCWAGE
+)
+
+df2 <- df %>% select(
+  YEAR,
+  SERIAL,
+  PERNUM,
+  EDUC,
+  DEGFIELD,
+  OCC, 
+  IND,
+  UHRSWORK
+)
+
+write.csv(df1, "IPUMS_ACS2019_CA_1.csv")
+write.csv(df2, "IPUMS_ACS2019_CA_2.csv")
+
+
